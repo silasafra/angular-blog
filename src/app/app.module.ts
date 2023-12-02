354fcfe74301dc588d973b682124ddc651fe6ae0
Silas Afra <silasafra@gmail.com>
2023-12-02 18:09:45 -0300

    createdes components and styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { ComponentsMenuBarComponent } from './componentes/menu-bar/menu-bar.component'
+import { BigCardComponent } from './componentes/big-card/big-card.component';
+import { PostCardComponent } from './componentes/post-card/post-card.component';
+import { AuthorPostComponent } from './componentes/author-post/author-post.component';
+import { HomeComponent } from './pages/home/home.component'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ComponentsMenuBarComponent, BigCardComponent, PostCardComponent, AuthorPostComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/componentes/menu-bar/menu-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'app-components-menu-bar',
+  templateUrl: './menu-bar.component.html',
+  styleUrls: ['./menu-bar.component.css'],
+})
+export class ComponentsMenuBarComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
